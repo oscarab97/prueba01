fd5fab09c0bfb19c1496ae25920a3a62fae2a57a
OscarOAB <233258@ust.com>
2024-10-11 22:40:34 -0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    int x= 3, y= 5;
+    int a = x+y-x/y+2;
+    int b=(x+y)-x/(y+2);
+    String s = null;
+    if (b<8 && Integer.parse(s)> 0){
+
+        System.out.println(a+b);
+}
+
+}
